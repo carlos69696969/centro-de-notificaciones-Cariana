caf538dbb3cd937b7e27f706ab87abab8d8ec567
Carlos Juarez <carianasoporte1@gmail.com>
2026-07-20 10:32:27 -0600

    feat: serve Android app links domain

--- src/server.js
+++ src/server.js
@@ -20,8 +20,38 @@ const storefrontNotificationsRoutes = require("./routes/storefrontNotifications"
 const abandonedCartRoutes = require("./routes/abandonedCart");
 
 const app = express();
+const APP_LINK_DOMAIN = "app.cariana.mx";
+const SHOP_DOMAIN = "www.cariana.mx";
+const PLAY_APP_SIGNING_SHA256 =
+  "C3:10:44:A6:80:8C:D7:C3:97:22:C6:21:BF:64:82:8C:A9:51:67:30:93:83:DE:62:B5:6B:3A:3F:72:56:69:00";
+
+function normalizeHost(hostHeader) {
+  return String(hostHeader || "")
+    .split(":")[0]
+    .toLowerCase();
+}
 
 app.use(cors());
+app.get("/.well-known/assetlinks.json", (_req, res) => {
+  res.type("application/json").send([
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "android_app",
+        package_name: "com.carlosjuarez.cariana",
+        sha256_cert_fingerprints: [PLAY_APP_SIGNING_SHA256],
+      },
+    },
+  ]);
+});
+app.get("*", (req, res, next) => {
+  if (normalizeHost(req.headers.host) !== APP_LINK_DOMAIN) {
+    return next();
+  }
+
+  const target = new URL(req.originalUrl || "/", `https://${SHOP_DOMAIN}`);
+  return res.redirect(302, target.toString());
+});
 app.use("/webhooks", webhooksRoutes);
 app.use(express.json({ limit: "1mb" }));
 app.use(resolveShopDomain);
